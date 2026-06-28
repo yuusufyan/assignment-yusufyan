3f2f429334d5baf31db94d0ea8684696c5fa0ae7
yuusufyan <yuusufyan13@gmail.com>
2026-06-28 21:05:32 +0700

    FEAT : Input material grade type

--- type/material-grade-type.js
+++ type/material-grade-type.js
@@ -1,3 +1,13 @@
+const GRADE_PROPERTIES = {
+    'MGP10': { E: 1100, E_90: 110, G: 687.5, G_90: 62.5 },
+    'MGP12': { E: 1100, E_90: 110, G: 687.5, G_90: 62.5 }
+};
+
 class MaterialGrade {
-    
+    static getProperties(grade) {
+        if (!GRADE_PROPERTIES[grade]) {
+            throw new Error(`Grade material ${grade} tidak ditemukan.`);
+        }
+        return GRADE_PROPERTIES[grade];
+    }
 }

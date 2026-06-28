d1168c2554d9a4db4f61c4d4c1734e2dc3e2d529
yuusufyan <yuusufyan13@gmail.com>
2026-06-28 21:06:12 +0700

    FEAT : Add Panel Property type

--- type/panel-properties-type.js
+++ type/panel-properties-type.js
@@ -1,5 +1,7 @@
 class PanelPropertiesType {
     constructor() {
-        this.layers = [];
+        this.EI_eff = 0; // Kekakuan Lentur Efektif
+        this.totalHeight = 0; // Total Ketebalan
+        this.info = ""; // Informasi atau status kalkulasi
     }
 }
